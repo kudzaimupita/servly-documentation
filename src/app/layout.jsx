27f02996b587514098df776c6e2bb597d0dcc846
Kudzai Mupita <kmupita@gmail.com>
2023-08-28 01:33:04 +0200

    first commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,8 +7,8 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
+    template: '%s - Servly API Reference',
+    default: 'Servly API Reference',
   },
 }
 
