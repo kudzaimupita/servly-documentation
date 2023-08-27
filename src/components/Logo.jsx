b87775461e3fa45c8210ef59ce829a42f58a008b
Kudzai Mupita <kmupita@gmail.com>
2023-08-28 01:46:57 +0200

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export function Logo(props) {
   return (
     <img
-      style={{ height: '40px' }}
+      style={{ height: '50px' }}
       src="https://www.servly.app/assets/img/8083535-ai.png"
     ></img>
   )
