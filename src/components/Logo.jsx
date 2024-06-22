9816e0dc353e1eefd8a607be59c4ba1a082f987e
kudzaimupita <kmupita@gmail.com>
2024-06-22 18:32:43 +0200

    add: ops view views

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export function Logo(props) {
   return (
     <img
       style={{ height: '50px' }}
-      src="https://www.servly.app/assets/img/8083535-ai.png"
+      src="https://servly.app/assets/img/logo/SERVLYLOGO.png"
     ></img>
   )
 }
